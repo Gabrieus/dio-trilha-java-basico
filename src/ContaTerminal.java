39d5f13aa385b9d64f936e3bc7285375cb00ffc3
Gabee <gabrielcoelhocastroppp09@gmail.com>
2023-10-20 20:45:39 -0300

    Projeto Banco Estudos

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // atributos
+
+        // TODO:Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // exibir a mensagem conta criada
+
+        int numConta;
+        String numAgencia = " ";
+        Double saldo = 237.48;
+        boolean status = true;
+        String nomeCliente = "Mario Andrade";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência ");
+
+        numAgencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da conta ");
+        numConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o seu nome ");
+        nomeCliente = sc.nextLine();
+        
+         System.out.println("Por favor, digite o seu saldo ");
+         saldo = sc.nextDouble();
+
+         System.out.println("Olá ," + nomeCliente + " obrigado por criar uma conta em nosso banco sua agência é " + numAgencia + " conta: " + numConta +" e seu saldo " + saldo + " já está disponível para saque ");
+
+         sc.close();
+
+          
+
+
+        
+
     }
 }
